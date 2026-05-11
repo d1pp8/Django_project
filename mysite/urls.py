b0a82117c8a26f7701fcbef28fa7e7a591e31190
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-05-11 23:08:27 +0200

    add new Route "home-page" where the greetings feature is activated

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from new_app.views import greetings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home-page/', greetings)
 ]
--- new_app/views.py
+++ new_app/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+def greetings(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello World!")
