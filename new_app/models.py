fe28df07e2ce439ff45c995fda3bb96e2c1e1531
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-05-14 23:31:45 +0200

    add 3 new models

--- new_app/models.py
+++ new_app/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from unicodedata import category
 
-# Create your models here.
+
+STAUS_OF_TASKS = [
+        ('new', 'New'),
+        ('in prog', 'In Progress'),
+        ('pend', 'Pending'),
+        ('bl', 'Blocked'),
+        ('dn', 'Done'),
+        ('na', 'N/A')
+    ]
+
+
+class Task(models.Model):
+
+    title = models.CharField(
+        max_length=255,
+        unique_for_date='created_at',
+        null=False,
+        blank=False
+    )
+
+    description = models.TextField()
+    categories = models.ManyToManyField('Category', related_name='tasks')
+
+    status = models.CharField(
+        max_length=10,
+        choices=STAUS_OF_TASKS,
+        default='na'
+    )
+
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Subtask(models.Model):
+
+    title = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    description = models.TextField()
+
+    task = models.ForeignKey(
+        'Task',
+        on_delete=models.CASCADE,
+        related_name='subtasks'
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=STAUS_OF_TASKS,
+        default='na'
+    )
+
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
